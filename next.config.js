a0f4873b5758a09009e6da0ae65ad8e32cb20c82
ArthurPhyto <arthur@swissharvest.com>
2024-12-26 02:07:03 +0100

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
